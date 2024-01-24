ccd98cda5f872f108c5270cb97a9e2d9ca42fc80
mrodriguezdev <marioguzman_99@hotmail.com>
2024-01-23 20:24:19 -0600

    Creando módulos
    Agregando módulo compartido. - Agregando router outlet para manejar los componentes en homeComponent. - Agregando módulo de autenticación

---
+++ src/app/modules/home/home-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomePageComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomeRoutingModule { }
